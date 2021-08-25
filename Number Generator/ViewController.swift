4777d0f527f5544e4424d0f7a1ca293175fa9c90
˜Roberto <roberto.jesus.amaral@gmail.com>
2021-08-25 17:24:49 -0300

    initial commit

--- Number Generator/ViewController.swift
+++ Number Generator/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var number: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberGenerator(_ sender: Any) {
+        
+        number.text = String(Int.random(in: 0..<1001))
+        
+    }
+    
 }
 
